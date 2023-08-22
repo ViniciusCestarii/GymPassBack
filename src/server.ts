4513369ddbdde8b39305f4f15cf1b939b0bb8173
Vinicius Cestari <viniciuscestari01@gmail.com>
2023-08-22 14:54:34 -0300

    creating enviroment variables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
   host: '0.0.0.0',
-  port: 3333,
+  port: env.PORT,
 }).then(() => {
-  console.log('Server listening on http://localhost:3333');
+  console.log(`Server listening on http://localhost:${env.PORT}`);
 })
